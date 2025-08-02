7401046d6ae49633184a87631697ab3968b3d908
Ayşenur Erkin <56235447+Aysenur-Erkin@users.noreply.github.com>
2025-08-02 16:47:55 +0300

    Add files via upload

---
+++ Öğrenci_Sınav_Notları/Öğrenci_Sınav_Notları/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Öğrenci_Sınav_Notları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad, soyad;
+            ad = textBox1.Text;
+            soyad = textBox2.Text;
+            int s1, s2, proje;
+            double ortalama;
+            s1 = Convert.ToInt32(textBox3.Text);
+            s2 = Convert.ToInt32(textBox4.Text);
+            proje = Convert.ToInt32(textBox5.Text);
+            ortalama = (s1 + s2 + proje) / 3;
+            listBox1.Items.Add(ad + "  " + soyad + "  Ortalama: " + ortalama);
+        }
+    }
+}
